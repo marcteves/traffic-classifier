2de2e5ad8630301ad312e0d681c411a6fa5b0d87
Marc Teves <gmarc.teves@gmail.com>
2019-10-16 22:28:44 +0800

    Implement edge filtering

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Classifier
+import           Control.Monad
 import qualified Data.Bits                        as B
 import qualified Data.Bits.Bitwise                as B
 import           Data.Maybe
@@ -11,6 +12,15 @@ import           System.Random
 
 main :: IO ()
 main = do
-    gen <- getStdGen
-    let trainingset = take 1000 . randomFlow $ gen
-    putStrLn . show . initGraphModel $ trainingset
+    gen <- getStdGen -- use same generator so that the first elements are shared
+    forM_ [2..15] $ printEdges gen
+
+printEdges gen int = do
+    let trainingset = take int . randomFlow $ gen
+    let graphmodel = initGraphModel trainingset
+    let edgesNow = edges graphmodel
+    let numEdges = length edgesNow
+    let edgeTuples = map edge edgesNow
+    let igrs = map igr edgesNow
+    putStrLn . show . filter ((==) 1.0 . snd) . zipWith (,) edgeTuples $ igrs
+    putStrLn $ "number of edges: " ++ show numEdges
--- src/Classifier.hs
+++ src/Classifier.hs
@@ -2,9 +2,9 @@ module Classifier where
 
 import qualified Data.Bits                        as B
 import qualified Data.Bits.Bitwise                as B
-import qualified Data.List as L
-import           Data.Word
+import qualified Data.List                        as L
 import           Data.Maybe
+import           Data.Word
 import           FlowGenerator
 import qualified Numeric.Probability.Distribution as D
 
@@ -12,17 +12,19 @@ type ProbDist = D.Distribution Float [Outcome]
 type CondProbDist = D.Distribution Float ([Outcome], [Outcome])
 type Outcome = Bool
 
+-- A node can represent multiple adjacent bit/byte offsets
 data Node = Node
-    { offsets :: [Int]
-    , index :: Int
+    { offsets  :: [Int]
+    , index    :: Int
     , probDist :: ProbDist
+    , entropy  :: Float
     } deriving Show
 
 data Edge = Edge
-    { edge :: (Int, Int)
-    , fstEntropy :: Float
-    , sndEntropy :: Float
+    { edge         :: (Int, Int)
+    , condEntropy  :: Float
     , condProbDist :: CondProbDist
+    , igr          :: Float
     } deriving Show
 
 data Graph = Graph
@@ -34,19 +36,12 @@ data Graph = Graph
 newGraph :: [Node] -> [Edge] -> Graph
 newGraph nodes edges = Graph nodes edges
 
-initNode :: Int -> Node
-initNode int =
-    let offsets = [int]
-        index = int
-        probDist = D.empty :: ProbDist
-    in  Node offsets index probDist
-
 initGraphModel :: [Flow] -> Graph
 initGraphModel trainingset =
     let outcomes = map B.toListBE trainingset
         numNodes = B.finiteBitSize . head $ trainingset
-        initNodes = map initNode [1..numNodes]
-        initGraph = newGraph initNodes []
+        initNodes = map (\int -> Node [int] int D.empty 0) [1..numNodes]
+        initGraph = Graph initNodes []
         {- Sort the training set now so functions can below can assume this -}
         sortedTrainingSet = L.sort trainingset
     in graphModel sortedTrainingSet initGraph
@@ -54,40 +49,87 @@ initGraphModel trainingset =
 -- iteratively refine graph model
 graphModel :: [Flow] -> Graph -> Graph
 graphModel trainingset graph =
-    let newNodes = map (nodeProbDist trainingset) $ nodes graph
-        nodePairs = filter ((2 == ) . length) $ L.subsequences $ nodes graph
-        condProbDists = map (nodeCondProbDist trainingset) $ nodePairs
-    in  Graph newNodes condProbDists
+    let newNodes = map (newNode trainingset) $ nodes graph
+        nodePairs = filter ((2 == ) . length) $ L.subsequences $ newNodes
+        allNodePairs =  nodePairs -- nodePairs ++ map reverse nodePairs
+        edgeCandidates = map (edgeCandidate trainingset) $ allNodePairs
+        strongEdges = filter ((==) 1.0 . igr) edgeCandidates -- use adaptive threshold next time
+        mergedNodes = mergeNodes strongEdges newNodes
+    in  Graph mergedNodes strongEdges
+
+mergeNodes :: [Edge] -> [Node] -> [Node]
+mergeNodes e n = n
+
+newNode :: [Flow] -> Node -> Node
+newNode ts n =
+    let pd = probDistFromFlows (offsets n) ts
+        entropy = computeNodeEntropy pd
+    in n {
+         probDist = pd
+       , entropy = entropy
+       }
 
 nodeProbDist :: [Flow] -> Node -> Node
 nodeProbDist trainingset node =
     node { probDist = probDistFromFlows (offsets node) trainingset }
 
-nodeCondProbDist :: [Flow] -> [Node] -> Edge
-nodeCondProbDist trainingset nodes =
-    let strip offsetList = map snd . filter (flip elem offsetList . fst) . zipWith (,) [1..]
-        nodeA = head nodes
+computeNodeEntropy :: ProbDist -> Float
+computeNodeEntropy pd =
+    let probabilities = map snd $ D.toList pd
+        information p = p * logBase 2 p
+    in  negate $ foldr (+) 0 $ map information probabilities
+
+edgeCandidate :: [Flow] -> [Node] -> Edge
+edgeCandidate trainingset nodes =
+    let nodeA = head nodes
         nodeB = last nodes
         nodeAIndex = index nodeA
         nodeBIndex = index nodeB
         offsetsA = offsets nodeA
         offsetsB = offsets nodeB
-        condProbDists = condProbDistFromFlows offsetsA offsetsB trainingset
-    in Edge (nodeAIndex, nodeBIndex) 0 0 condProbDists
+        condProbDist = condProbDistFromFlows offsetsA offsetsB trainingset
+        condEntropy = computeCondEntropy (probDist nodeA) condProbDist
+        igr = computeIGR (map entropy [nodeA, nodeB]) condEntropy
+    in Edge (nodeAIndex, nodeBIndex) condEntropy condProbDist igr
+
+computeIGR :: [Float] -> Float -> Float
+computeIGR es ce =
+    let minEntropy = minimum es
+        xv = head es
+        num = xv - ce
+    in  if (abs minEntropy < 0.001) then 1.0 else
+        if (abs num < 0.001) then 0 else
+        num / minEntropy
+
+computeCondEntropy :: ProbDist -> CondProbDist -> Float
+computeCondEntropy pd cpd =
+    let listpd = L.sortOn fst $ D.toList pd
+        compareFirstOutcome a b = (fst . fst) a == (fst . fst) b
+        listcpd = L.groupBy compareFirstOutcome $ L.sortOn (fst . fst) $ D.toList cpd
+        information x xy = xy * logBase 2 (x / xy)
+        pdpsonly = map snd listpd
+        cpdpsonly = map (map snd) listcpd
+    in  foldr (+) 0 $ concat $ zipWith (\x -> map (information x)) pdpsonly cpdpsonly
 
 probDistFromFlows :: [Int] -> [Flow] -> ProbDist
 probDistFromFlows offsetList trainingset =
-    let indexedFlow = zipWith (,) [1..] . B.toListBE
-        filteredFlow = map snd . filter (flip elem offsetList . fst)
-        strippedTrainingSet = map (filteredFlow . indexedFlow) trainingset
+    let strippedTrainingSet = stripTrainingset offsetList trainingset
         groupedTrainingSet = L.group strippedTrainingSet
-    in  fromJust . D.normalize . D.fromList . zipWith (,) (map head groupedTrainingSet) $ (map (toEnum . length) groupedTrainingSet)
+        headOfGroups = map head groupedTrainingSet
+        lengthOfGroups = map (toEnum . length) groupedTrainingSet
+    in  fromJust . D.normalize . D.fromList . zipWith (,) headOfGroups $ lengthOfGroups
 
 condProbDistFromFlows :: [Int] -> [Int] -> [Flow] -> CondProbDist
 condProbDistFromFlows offsetA offsetB trainingset =
+    let strippedTrainingSetA = stripTrainingset offsetA trainingset
+        strippedTrainingSetB = stripTrainingset offsetB trainingset
+        groupedTrainingSet = L.group $ zipWith (,) strippedTrainingSetA strippedTrainingSetB
+        headOfGroups = map head groupedTrainingSet
+        lengthOfGroups = map (toEnum . length) groupedTrainingSet
+    in  fromJust . D.normalize . D.fromList . zipWith (,) headOfGroups $ lengthOfGroups
+
+stripTrainingset offsetList trainingset =
     let indexedFlow = zipWith (,) [1..] . B.toListBE
-        filteredFlow o = map snd . filter (flip elem o. fst)
-        strippedTrainingSetA = map (filteredFlow offsetA . indexedFlow) trainingset
-        strippedTrainingSetB = map (filteredFlow offsetB . indexedFlow) trainingset
-        groupedTrainingSet = L.group (zipWith (,) strippedTrainingSetA strippedTrainingSetB)
-    in  fromJust . D.normalize . D.fromList . zipWith (,) (map head groupedTrainingSet) $ (map (toEnum . length) groupedTrainingSet)
+        filteredFlow = map snd . filter (flip elem offsetList. fst)
+    in  map (filteredFlow . indexedFlow) trainingset
+
