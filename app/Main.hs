58273961d7bc78a1609971f98aa707100769b675
Marc Teves <gmarc.teves@gmail.com>
2019-10-17 15:24:06 +0800

    Print removed edges per iteration

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import           Control.Monad
 import qualified Data.Bits                        as B
 import qualified Data.Bits.Bitwise                as B
 import           Data.Maybe
+import qualified Data.List                        as L
 import           Data.Word
 import           FlowGenerator
 import qualified Numeric.Probability.Distribution as D
@@ -13,14 +14,22 @@ import           System.Random
 main :: IO ()
 main = do
     gen <- getStdGen -- use same generator so that the first elements are shared
-    forM_ [2..15] $ printEdges gen
+    let trainingset = randomFlow $ gen
+    firstTuples <- printEdges trainingset 1
+    result <- forM [2..15] $ printEdges trainingset
+    foldM_ compareEdges firstTuples result
 
-printEdges gen int = do
-    let trainingset = take int . randomFlow $ gen
+printEdges ts int = do
+    let trainingset = take int ts
     let graphmodel = initGraphModel trainingset
     let edgesNow = edges graphmodel
-    let numEdges = length edgesNow
     let edgeTuples = map edge edgesNow
     let igrs = map igr edgesNow
-    putStrLn . show . filter ((==) 1.0 . snd) . zipWith (,) edgeTuples $ igrs
+    return edgeTuples
+
+compareEdges a b = do
+    let numEdges = length b
+    putStrLn $ "Edges of graph:" ++ show b
     putStrLn $ "number of edges: " ++ show numEdges
+    putStrLn $ "Edges removed from past iteration:" ++ (show $ a L.\\ b)
+    return b
